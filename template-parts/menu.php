3545ef32d466f159117cee3535712f3b285d2513
sakhawathusain2019 <sakhawathusain2019@gmail.com>
2019-05-30 11:47:51 +0600

    Menu.php Something Menu function add

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -10,6 +10,7 @@ function ucorpa_nav_menu(){
 // replace Sub Menu Class for Wp Default sub menu.
 function change_submenu_class($menu) {
     $menu = preg_replace('/ class="sub-menu"/','/ class="dropdown" /',$menu);
+    $menu = preg_replace('/ class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-has-children menu-item-5"/','/ class="current" /',$menu);
     return $menu;
 }
 add_filter('wp_nav_menu','change_submenu_class');
